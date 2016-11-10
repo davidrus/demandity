e05ed56049110b1eb6c31280c96240b2afe26870
david.rus <david.rus@embedit.cz>
2016-11-10 15:39:13 +0100

    feat(app): Hello world app using react with hot module reload

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from './containers/App';
+
+const { AppContainer } = require('react-hot-loader');
+
+// Get the root element from the HTML
+const rootEl = document.getElementById('app');
+
+// Tell Typescript that there is a global variable called module - see below
+declare var module: { hot: any };
+
+// And render our App into it, inside the HMR App ontainer which handles the hot reloading
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
+);
+
+// Handle hot reloading requests from Webpack
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    // If we receive a HMR request for our App container, then reload it using require (we can't do this dynamically with import)
+    const NextApp = require('./containers/App').default;
+
+    // And render it into the root element again
+    render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      rootEl
+    );
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,108 @@
-module.export = () => {
+const { resolve } = require('path');
+
+const webpack = require('webpack');
+
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+// webpack config helpers
+const { getIfUtils, removeEmpty, combineLoaders } = require('webpack-config-utils');
+
+module.exports = (env) => {
+  const { ifProd, ifNotProd } = getIfUtils(env);
 
   return {
-
-    context: __dirname,
-    entry: './index',
+    context: resolve('src'),
+    entry: {
+      app: './main.tsx'
+    },
     output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
+      filename: '[name].js',
+      path: resolve('dist'),
+      // Include comments with information about the modules.
+      pathinfo: ifNotProd(),
+      libraryTarget: 'umd'
     },
 
     resolve: {
       extensions: [
         '.js',
-        '.ts'
+        '.ts',
+        '.tsx'
       ]
     },
 
-    devtool: 'cheap-module-source-map',
+    devtool: ifProd('source-map', 'cheap-module-source-map'),
 
     module: {
       loaders: [
-        { text: /\.ts$/, loaders: ['awesome-typescript-loader']}
-      ]
+        // Typescript
+        { test: /\.tsx?$/, loaders: [ 'awesome-typescript-loader' ], exclude: /node_modules/ },
+        // CSS
+        {
+          test: /\.css$/,
+          loaders: combineLoaders( [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                sourceMap: true
+              }
+            },
+            { loader: 'postcss-loader' }
+          ] )
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
+        }
+      ],
     },
 
-    devServer: {
-      historyApiFallback: true,
-      host: '0.0.0.0',
-      proxy: {
-        '/api': {
-          target: 'http://localhost:90000'
+    // This is required, when using Hot Code Replacement between multiple calls to the compiler.
+    recordsPath: resolve(__dirname, './tmp/webpack-records.json'),
+
+    plugins: removeEmpty([
+
+      // Add nice progress bar
+      new ProgressBarPlugin(),
+
+      new HtmlWebpackPlugin({
+        template: resolve('src','index.html')
+      }),
+
+      // Set NODE_ENV to enable production react version
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: ifProd('"production"', '"development"')
         }
-      }
-    }
+      }),
 
+      // Deduplicate node modules dependencies
+      ifProd(new webpack.optimize.DedupePlugin()),
+
+      // // Default webpack build options
+      // ifProd(new webpack.LoaderOptionsPlugin({
+      //   debug: false
+      // })),
+
+      // // Uglify bundles
+      // ifProd(new webpack.optimize.UglifyJsPlugin({
+      //   compress: {
+      //     warnings: false,
+      //   },
+      //   output: {
+      //     comments: false
+      //   }
+      // }))
+
+    ])
   }
-
-}
+};
